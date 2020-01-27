73a22591922e3f1c187331b78000bc31364e071d
Payne Chang <46759126+paynechang@users.noreply.github.com>
2020-01-26 21:20:02 -0500

    Add files via upload

---
+++ Research/NeuronSimulation/Introduction/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>NEURON Introduction</title>
+	<meta content="text/html;charset=UTF-8" />
+    <link href="../style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <h1 style="margin-bottom: 0px">
+        NEURON Simulation</h1>
+    <hr />
+    <table style="border-style: none">
+        <tr>
+            <td style="border-style: none">
+                <?php include("../navigationBar.php"); ?>
+            </td>
+            <td style="border-style: none">
+                <h2>
+                    Introduction</h2>
+                <p>
+                    NEURON is a programming environment for simulating neuronal activity and more. 
+                    There is a big community among research institutes and on the internet.</p>
+                <p>
+                    My projects are focusing on hyperpolarization-activated cation channels (HCN) 
+                    and Kv1.1 channels, which contribute to I<sub>h</sub> and I<sub>D</sub>, 
+                    respectively. I organized some resources and my notes here. I hope you might 
+                    find them useful.</p>
+            </td>
+        </tr>
+    </table>
+    <hr />
+</body>
+</html>
---
+++ Research/NeuronSimulation/navigationBar.php
@@ -0,0 +1,5 @@
+<ul class="ul">
+    <li class="li"><a class="a" href="../Introduction/index.php">Introduction</a></li>
+    <li class="li"><a class="a" href="../Notes/index.php">Notes</a></li>
+    <li class="li"><a class="a" href="../Classes/index.php">Classes</a></li>
+</ul>
