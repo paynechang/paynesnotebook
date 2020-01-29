426dc2b0da5399cdf3d619a9935bf474da23ac5d
Payne Chang <46759126+paynechang@users.noreply.github.com>
2020-01-28 20:38:26 -0500

    Delete navigationBar.php

--- Research/NeuronSimulation/navigationBar.php
+++
@@ -1,5 +0,0 @@
-<ul class="ul">
-    <li class="li"><a class="a" href="../Introduction/index.php">Introduction</a></li>
-    <li class="li"><a class="a" href="../Notes/index.php">Notes</a></li>
-    <li class="li"><a class="a" href="../Classes/index.php">Classes</a></li>
-</ul>
